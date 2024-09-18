3f6f6fe6847beee32385684395936a47e5934dd4
alsandre <gabeditebuli@gmail.com>
2024-09-18 18:33:38 +0400

    feat: setup filters layout

--- src/components/FiltersPanel.tsx
+++ src/components/FiltersPanel.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { EFilters, Filters, IFiltersPanelProps } from "../types";
 import { useForm } from "react-hook-form";
 import { CheckboxFilter } from "./CheckBoxFilter";
@@ -14,26 +14,58 @@ export const FiltersPanel: React.FC<IFiltersPanelProps> = () => {
   const { register, watch, setValue } = useForm<Filters>({
     defaultValues: FILTERS_FORM_DEFAULT_VALUES,
   });
+  const [isRegionsFilterOpen, setIsRegionsFilterOpen] = useState(false);
+  const [isPriceFilterOpen, setIsPriceFilterOpen] = useState(false);
+  const [isAreaFilterOpen, setIsAreaFilterOpen] = useState(false);
+  const [isBedroomsFilterOpen, setIsBedroomsFilterOpen] = useState(false);
   const fields = watch();
   console.log(fields);
 
   return (
     <>
-      <div>
-        <CheckboxFilter register={register} fieldName={EFilters.REGIONS} />
-        <RangeFilter
-          register={register}
-          setValue={setValue}
-          fieldName="price"
-          range={PRICE_RANGES}
-        />
-        <RangeFilter
-          register={register}
-          setValue={setValue}
-          fieldName="area"
-          range={AREA_RANGES}
-        />
-        <InputFilter register={register} fieldName={EFilters.BEDROOMS} />
+      <div className="flex">
+        <div>
+          <span onClick={() => setIsRegionsFilterOpen((prev) => !prev)}>
+            რეგიონი
+          </span>
+          {isRegionsFilterOpen && (
+            <CheckboxFilter register={register} fieldName={EFilters.REGIONS} />
+          )}
+        </div>
+        <div>
+          <span onClick={() => setIsPriceFilterOpen((prev) => !prev)}>
+            საფასო კატეგორია
+          </span>
+          {isPriceFilterOpen && (
+            <RangeFilter
+              register={register}
+              setValue={setValue}
+              fieldName="price"
+              range={PRICE_RANGES}
+            />
+          )}
+        </div>
+        <div>
+          <span onClick={() => setIsAreaFilterOpen((prev) => !prev)}>
+            ფართობი
+          </span>
+          {isAreaFilterOpen && (
+            <RangeFilter
+              register={register}
+              setValue={setValue}
+              fieldName="area"
+              range={AREA_RANGES}
+            />
+          )}
+        </div>
+        <div>
+          <span onClick={() => setIsBedroomsFilterOpen((prev) => !prev)}>
+            საძინებლების რაოდენობა
+          </span>
+          {isBedroomsFilterOpen && (
+            <InputFilter register={register} fieldName={EFilters.BEDROOMS} />
+          )}
+        </div>
       </div>
     </>
   );
