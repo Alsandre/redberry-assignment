5ce9c4ac662ce481bbf543b45f593ee2f9f5cade
alsandre <gabeditebuli@gmail.com>
2024-09-19 23:22:03 +0400

    refactor: make modular button

---
+++ src/components/PrimaryBtn.tsx
@@ -0,0 +1,36 @@
+import { ReactNode } from "react";
+import { Button } from "./ui/Button";
+
+const variantStyles = {
+  GHOST:
+    "text-rdbryPrimary-100 border border-solid border-1 border-rdbryPrimary-100 bg-rdbryText-100 px-4 py-[10px] rounded-[10px] font-medium",
+  DEFAULT:
+    "text-rdbryText-100 bg-rdbryPrimary-100 px-4 py-[10px] rounded-[10px] font-medium",
+};
+export enum EPrimaryButtonVariants {
+  GHOST = "GHOST",
+  DEFAULT = "DEFAULT",
+}
+
+interface IPrimaryBtnProps {
+  icon: ReactNode;
+  variant: EPrimaryButtonVariants;
+  label: string;
+  onClick: () => void;
+}
+
+export const PrimaryBtn: React.FC<IPrimaryBtnProps> = ({
+  icon,
+  variant,
+  label,
+  onClick,
+}) => {
+  return (
+    <>
+      <Button className={variantStyles[variant]} onClick={onClick}>
+        {icon ?? ""}
+        {label}
+      </Button>
+    </>
+  );
+};
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,6 +6,7 @@ import { IFilters, IGetEstatesList } from "../types";
 import { handleFiltering } from "../utils/handleFiltering";
 import { Button } from "../components/ui/Button";
 import { PlusIcon } from "../components/icons";
+import { EPrimaryButtonVariants, PrimaryBtn } from "../components/PrimaryBtn";
 
 export const HomePage = (): JSX.Element => {
   const { data, isLoading, isError, refetch } = useEstatesList();
@@ -29,12 +30,18 @@ export const HomePage = (): JSX.Element => {
       <div className="flex justify-between">
         <FiltersPanel onFilterChange={handleFilterchange} />
         <div className="flex gap-4">
-          <Button className="text-rdbryText-100 bg-rdbryPrimary-100 px-4 py-[10px] rounded-[10px] font-medium">
-            <PlusIcon /> ლისტინგის დამატება
-          </Button>
-          <Button className="text-rdbryPrimary-100 border border-solid border-1 border-rdbryPrimary-100 bg-rdbryText-100 px-4 py-[10px] rounded-[10px] font-medium">
-            <PlusIcon /> აგენტის დამატება
-          </Button>
+          <PrimaryBtn
+            variant={EPrimaryButtonVariants.DEFAULT}
+            onClick={() => ""}
+            label="ლისტინგის დამატება"
+            icon={<PlusIcon />}
+          />
+          <PrimaryBtn
+            variant={EPrimaryButtonVariants.GHOST}
+            onClick={() => ""}
+            label="აგენტის დამატება"
+            icon={<PlusIcon />}
+          />
         </div>
       </div>
       <EstateList {...{ data: filteredData, isError, isLoading, refetch }} />
