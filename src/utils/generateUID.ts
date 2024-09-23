d7a3dc5ab59a6879230119c3eb1ef28d16787c16
alsandre <gabeditebuli@gmail.com>
2024-09-23 11:38:35 +0400

    fix: decrease UUID duplication risks

--- src/utils/generateUID.ts
+++ src/utils/generateUID.ts
@@ -1,3 +1,3 @@
 export const generateUID = () => {
-  return `id-${Date.now()}-${Math.floor(Math.random() * 1000)}`;
+  return `id-${Date.now()}-${Math.floor(Math.random() * 100000)}-${Math.floor(Math.random() * 100000)}`;
 };
